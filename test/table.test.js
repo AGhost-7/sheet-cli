b78e9377ec71ee66ab349de47b02f8f8d683551d
AGhost-7 <jonathan.boudreau.92@gmail.com>
2017-02-09 21:20:42 -0500

    Implement padding

--- test/table.test.js
+++ test/table.test.js
@@ -1,24 +1,42 @@
 
 const table = require('../lib/table')
-const State = table.State
+const TableRenderer = table.TableRenderer
 const assert = require('assert')
 
 describe('table', () => {
 
-	const expect = [
-		['┌', '─', '┬', '─', '┬', '──', '┐'],
-		['│', 'a', '│', 'b', '│', 'cc', '│'],
-		['└', '─', '┴', '─', '┴', '──', '┘']
+	const data1 = [
+		['a a', 'bbbb', 'c', ''],
+		['d', 'd', 'ee', 'Ｆ']
 	]
-	const drawn = expect.map((row) => row.join('')).join('\n')
-	it('renders the borders\n' + drawn, () => {
-		const st = new State()
+	const renderer = new TableRenderer()
+
+	it('calculates the column widths', () => {
+		assert.deepEqual(renderer.columnWidths(data1), [3, 4, 2, 2])
+	})
+
+	it('pads the data', () => {
+		const expect = [
+			['a a', 'bbbb', 'c ', '  '],
+			['d  ', 'd   ', 'ee', 'Ｆ']
+		]
+
+		assert.deepEqual(renderer.padRows(data1), expect)
+	})
+
+	it.skip('renders the borders', () => {
+		const expect = [
+			['┌', '─', '┬', '─', '┬', '──', '┐'],
+			['│', 'a', '│', 'b', '│', 'cc', '│'],
+			['└', '─', '┴', '─', '┴', '──', '┘']
+		]
+		//const drawn = expect.map((row) => row.join('')).join('\n')
 		const data = [
 			['a', 'b', 'cc']//,
 			//['d', 'f', 'gg'],
 			//['h', 'i', 'jj']
 		]
 		//console.log(expect)
-		assert.deepEqual(st.withBorders(data), expect)
+		assert.deepEqual(renderer.withBorders(data), expect)
 	})
 })
